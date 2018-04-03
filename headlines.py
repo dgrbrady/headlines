3178217c2390d5ef18873cfd3e12729d696b810c
David Brady <dgrbrady@gmail.com>
2018-04-03 15:27:18 -0400

    added weather data via openweathermap API

--- headlines.py
+++ headlines.py
@@ -2,6 +2,9 @@ import feedparser
 from flask import Flask, render_template, request
 import pdb
 from flask_bootstrap import Bootstrap
+import json
+from urllib.request import urlopen
+import urllib.parse
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -24,8 +27,24 @@ def get_news():
     else:
         publication = query.lower()
     feed = feedparser.parse(NYT_FEEDS[publication])
+    weather = get_weather("London,UK")
 
-    return render_template("newsPage.html", headline=publication, articles=feed['entries'])
+    return render_template("newsPage.html", headline=publication, articles=feed['entries'], weather=weather)
 
+def get_weather(query):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid=be89569744a2e70fd838a538a7e69cb9"
+    query = urllib.parse.quote(query)
+    url = api_url.format(query)
+    data = urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+
+    if parsed.get("weather"):
+        weather = {
+            'description': parsed["weather"][0]["description"],
+            'temperature': parsed["main"]["temp"],
+            'city': parsed["name"]
+        }
+        return weather
 if __name__ == '__main__':
     app.run(port=5000, debug=True)

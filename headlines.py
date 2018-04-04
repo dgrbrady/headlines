581b91f7840e971cb72bc89e44b8aaceeeb4ac26
David Brady <dgrbrady@gmail.com>
2018-04-04 11:10:10 -0400

    Added currency drop-down selectors

--- headlines.py
+++ headlines.py
@@ -55,9 +55,9 @@ def home():
     currency_to = request.args.get('currency_to')
     if not currency_to:
         currency_to = DEFAULTS['currency_to']
-    rate = get_rate(currency_from, currency_to)
+    rate, currencies = get_rate(currency_from, currency_to)
 
-    return render_template("newsPage.html", articles=articles, weather=weather, currency_from=currency_from, currency_to=currency_to, rate=rate)
+    return render_template("newsPage.html", articles=articles, weather=weather, currency_from=currency_from, currency_to=currency_to, rate=rate, currencies=sorted(currencies))
 
 def get_news(query):
     if not query or query.lower() not in NYT_FEEDS:
@@ -88,7 +88,7 @@ def get_rate(frm, to):
     frm_rate = parsed.get(frm.upper())
     to_rate = parsed.get(to.upper())
 
-    return to_rate/frm_rate
+    return (to_rate / frm_rate, parsed.keys())
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
